a96ec687d3a2d1dad8582d12d638cd4279abecfa
Ashish Kumar Sinha <ashish97.kumar@gmail.com>
2017-03-30 00:12:42 +0530

    Change Form Layout and Added new Input IDs

--- js/contest.js
+++ js/contest.js
@@ -54,11 +54,8 @@
 			        var objTo = document.getElementById('l_additionalFields');
 			        var divtest = document.createElement("div");
 			        var x = l_info+1;
-			        if(l_info == 1){
-		        		$("#lfirstmember").prepend('<div class="col-md-12 form-group row" style="margin-top: 20px;margin-left:5px;" id="lteam"><label for="example-text-input" class="col-2 col-form-label">Team-Name</label><div class="col-10"><input class="form-control" type="text" placeholder="Name of the Team" id="example-text-input"></div></div>');
-		        	}
-			        divtest.innerHTML = '<div class="form-group row" style="margin-top: 20px;" id="l"><label for="example-text-input" class="col-2 col-form-label">Participant '+x+'</label><div class="col-10"><input class="form-control" type="text" placeholder="Name of the participant" id="example-text-input"></div></div>';
-			        
+			        divtest.innerHTML = '<div class="form-group row l" style="margin-top: 20px;" id="launch-email-'+x+'"><label for="example-text-input" class="col-2 col-form-label">Participant '+x+'</label><div class="col-10"><input class="form-control" type="text" placeholder="email-id" id="example-text-input"></div></div>';
+			        console.log(divtest.innerHTML)
 			        objTo.appendChild(divtest);
 			        l_info++;
 			        	if(l_info >= 5){
@@ -69,15 +66,15 @@
     				function l_remove_fields(){
     					if(l_info == 1){
     						$("#l_less_fields").hide();
-    						$('div#lteam').remove();
+    						
     					}
     					else{
     						$("#l_more_fields").show();
-    						$('div#l:last').remove();
+    						$('div.l:last').remove();
     						l_info--;
     						if(l_info == 1){
     							$("#l_less_fields").hide();
-    							$('div#lteam').remove();
+    							
     						}
     					}
     					
@@ -92,10 +89,7 @@
 			        var objTo = document.getElementById('additionalFields');
 			        var divtest = document.createElement("div");
 			        var x = info+1;
-			        if(info == 1){
-		        		$("#firstmember").prepend('<div class="col-md-12 form-group row" style="margin-top: 20px;margin-left:5px;" id="team"><label for="example-text-input" class="col-2 col-form-label">Team-Name</label><div class="col-10"><input class="form-control" type="text" placeholder="Name of the Team" id="example-text-input"></div></div>');
-		        	}
-			        divtest.innerHTML = '<div class="form-group row" style="margin-top: 20px;" id="p"><label for="example-text-input" class="col-2 col-form-label">Participant '+x+'</label><div class="col-10"><input class="form-control" type="text" placeholder="Name of the participant" id="example-text-input"></div></div>';
+			        divtest.innerHTML = '<div class="form-group row p" style="margin-top: 20px;" id="homepage-email-'+x+'"><label for="example-text-input" class="col-2 col-form-label">Participant '+x+'</label><div class="col-10"><input class="form-control" type="text" placeholder="email-id" id="example-text-input"></div></div>';
 			        console.log(divtest.innerHTML);
 			        objTo.appendChild(divtest);
 			        info++;
@@ -111,92 +105,15 @@
     					}
     					else{
     						$("#more_fields").show();
-    						$('div#p:last').remove();
+    						$('div.p:last').remove();
     						info--;
     						if(info == 1){
     							$("#less_fields").hide();
-    							$('div#team').remove();
+    							
     						}
     					}
     					
     				}
-	var cs_info = 1;
-				function cs_add_fields() {
-    			if(cs_info >= 5){
-    				$("#cs_more_fields").hide();
-    			}
-    			else{
-    				$("#cs_less_fields").show();
-		        var objTo = document.getElementById('cs_additionalFields');
-		        var divtest = document.createElement("div");
-		        var x = cs_info+1;
-		        if(cs_info == 1){
-		        		$("#csfirstmember").prepend('<div class="col-md-12 form-group row" style="margin-top: 20px;margin-left:5px;" id="csteam"><label for="example-text-input" class="col-2 col-form-label">Team-Name</label><div class="col-10"><input class="form-control" type="text" placeholder="Name of the Team" id="example-text-input"></div></div>');
-		        	}
-		        divtest.innerHTML = '<div class="form-group row" style="margin-top: 20px;" id="cs"><label for="example-text-input" class="col-2 col-form-label">Participant '+x+'</label><div class="col-10"><input class="form-control" type="text" placeholder="Name of the participant" id="example-text-input"></div></div>';
-		        console.log(divtest.innerHTML);
-		        objTo.appendChild(divtest);
-		        cs_info++;
-		        	if(cs_info >= 5){
-		        		$("#cs_more_fields").hide();
-		        	}
-		    		}
-				}
-				function cs_remove_fields(){
-					if(cs_info == 1){
-						$("#cs_less_fields").hide();
-						$('div#csteam').remove();
-					}
-					else{
-						$("#cs_more_fields").show();
-						$('div#cs:last').remove();
-						cs_info--;
-						if(cs_info == 1){
-							$("#cs_less_fields").hide();
-							$('div#csteam').remove();
-						}
-					}
-					
-				}
-	var cod_info = 1;
-				function cod_add_fields() {
-    			if(cod_info >= 5){
-    				$("#cod_more_fields").hide();
-    			}
-    			else{
-    				$("#cod_less_fields").show();
-		        var objTo = document.getElementById('cod_additionalFields');
-		        var divtest = document.createElement("div");
-		        var x = cod_info+1;
-		        	if(cod_info == 1){
-		        		$("#codfirstmember").prepend('<div class="col-md-12 form-group row" style="margin-top: 20px;margin-left:5px;" id="codteam"><label for="example-text-input" class="col-2 col-form-label">Team-Name</label><div class="col-10"><input class="form-control" type="text" placeholder="Name of the Team" id="example-text-input"></div></div>');
-		        	}
-		        divtest.innerHTML = '<div class="form-group row" style="margin-top: 20px;" id="cod"><label for="example-text-input" class="col-2 col-form-label">Participant '+x+'</label><div class="col-10"><input class="form-control" type="text" placeholder="Name of the participant" id="example-text-input"></div></div>';
-		        console.log(divtest.innerHTML);
-		        objTo.appendChild(divtest);
-		        cod_info++;
-		        	if(cod_info >= 5){
-		        		$("#cod_more_fields").hide();
-		        	}
-		    		}
-				}
-				function cod_remove_fields(){
-					if(cod_info == 1){
-						$("#cod_less_fields").hide();
-						$('div#codteam').remove();
-					}
-					else{
-						$("#cod_more_fields").show();
-						$('div#cod:last').remove();
-						cod_info--;
-						if(cod_info == 1){
-							$("#cod_less_fields").hide();
-							$('div#codteam').remove();
-						}
-					}
-					
-				}
 
-$('#gamingDropdownMenu #game').click(function (e) {
-    $('.active').removeClass('active');
-});
+
+
